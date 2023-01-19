e4b0946c4d4a0c3e92ae221d8ed4854b665ffb0b
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-01-19 23:03:04 +0300

    added_new_window_link

--- portfolio/src/components/Cards.vue
+++ portfolio/src/components/Cards.vue
@@ -5,117 +5,117 @@
   >
     <div
       id="littleCardGit"
-      class="transition squareTransitionFunc duration-[5s] z-[25] bg-[#ffffff2f] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-8rem] translate-y-[-9rem] rotate-[8deg] hover:scale-110 duration-[1s] cursor-pointer delay-[2s]': isClicked}"
+      class="transition squareTransitionFunc duration-[5s] z-[15] bg-[#ffffff2f] w-20 aspect-square rounded-xl absolute"
+      :class="{'translate-x-[-8rem] translate-y-[-9rem] rotate-[8deg] hover:scale-110 duration-[1s] cursor-pointer delay-[2s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
-    <a href="https://github.com/Inoooooor">
+    <a href="https://github.com/Inoooooor" target="_blank">
       <img src="../images/github.png" class="h-[100%] w-[100%]" alt="GitHub">
     </a>
     </div>
     <div
       id="littleCardTs"
       class="transition squareTransitionFunc duration-[5s] z-[35] bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[14rem] translate-y-[-7.5rem] rotate-[-2deg] hover:scale-110 duration-[1s]': isClicked}"
+      :class="{'translate-x-[14rem] translate-y-[-7.5rem] rotate-[-2deg] hover:scale-110 duration-[1s]': isClicked, 'delay-[0s]': isOnPosition }"
     ></div>
     <div
       id="littleCardVue"
       class="transition squareTransitionFunc duration-[5s] z-[15] bg-[#000000] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-12rem] translate-y-[11rem] rotate-[-4deg] hover:scale-110 duration-[1s] delay-[4s]': isClicked}"
+      :class="{'translate-x-[-12rem] translate-y-[11rem] rotate-[-4deg] hover:scale-110 duration-[1s] delay-[4s]': isClicked, 'delay-[0s]': isOnPosition }"
     ></div>
     <div
       id="littleCardTailwind"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-1rem] translate-y-[-14rem] rotate-[-4deg] hover:scale-110 duration-[1s] delay-[1.2s]': isClicked}"
+      :class="{'translate-x-[-1rem] translate-y-[-14rem] rotate-[-4deg] hover:scale-110 duration-[1s] delay-[1.2s]': isClicked, 'delay-[0s]': isOnPosition }"
     ></div>
     <div
       id="littleCardVite"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-14rem] translate-y-[15rem] rotate-[-14deg] hover:scale-110 duration-[1s] delay-[4.1s]': isClicked}"
+      :class="{'translate-x-[-14rem] translate-y-[15rem] rotate-[-14deg] hover:scale-110 duration-[1s] delay-[4.1s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/vite.png" alt="Vite">
     </div>
     <div
       id="littleCardPinia"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-8rem] translate-y-[13rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[4.2s]': isClicked}"
+      :class="{'translate-x-[-8rem] translate-y-[13rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[4.2s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/pinia.png" alt="Pinia" class="rounded-xl">
     </div>
     <div
       id="littleCardfirebase"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-10rem] translate-y-[17rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[4.3s]': isClicked}"
+      :class="{'translate-x-[-10rem] translate-y-[17rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[4.3s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/firebase.png" alt="firebase" class="rounded-xl">
     </div>
     <div
       id="littleCardfigma"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-22rem] translate-y-[-3.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3s]': isClicked}"
+      :class="{'translate-x-[-22rem] translate-y-[-3.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/figma.png" alt="figma" class="rounded-xl">
     </div>
     <div
       id="littleCardphotoshop"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-22rem] translate-y-[1rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3.1s]': isClicked}"
+      :class="{'translate-x-[-22rem] translate-y-[1rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3.1s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/photoshop.png" alt="photoshop" class="rounded-xl">
     </div>
     <div
       id="littleCardillustrator"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-22rem] translate-y-[5.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3.2s]': isClicked}"
+      :class="{'translate-x-[-22rem] translate-y-[5.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[3.2s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/illustrator.png" alt="illustrator" class="rounded-xl">
     </div>
     <div
       id="littleCardBootstrap"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[4rem] translate-y-[-15rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[1s]': isClicked}"
+      :class="{'translate-x-[4rem] translate-y-[-15rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[1s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/bootstrap.png" alt="Bootstrap" class="rounded-xl">
     </div>
     <div
       id="littleCardsass"
       class="transition squareTransitionFunc duration-[5s] z-[15] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[-2rem] translate-y-[-17.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[1.1s]': isClicked}"
+      :class="{'translate-x-[-2rem] translate-y-[-17.5rem] rotate-[14deg] hover:scale-110 duration-[1s] delay-[1.1s]': isClicked, 'delay-[0s]': isOnPosition }"
     >
       <img src="../images/sass.png" alt="sass" class="rounded-xl">
     </div>
     <div
       id="littleCardHs"
-      class="transition squareTransitionFunc duration-[5s] z-[35] bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[13rem] translate-y-[13.5rem] rotate-[12deg] hover:scale-110 duration-[1s] delay-[5s]': isClicked}"
+      class="transition squareTransitionFunc duration-[5s] z-[15] bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
+      :class="{'translate-x-[13rem] translate-y-[13.5rem] rotate-[12deg] hover:scale-110 duration-[1s] delay-[5s]': isClicked, 'delay-[0s]': isOnPosition }"
     ></div>
     <div
       id="littleCardCheckers"
-      class="transition squareTransitionFunc duration-[5s] z-[35] bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
-      :class="{'translate-x-[7rem] translate-y-[15rem] rotate-[-12deg] hover:scale-110 duration-[1s] cursor-pointer delay-[5.1s]': isClicked, 'delay-75': isOnPosition}"
+      class="transition squareTransitionFunc duration-[5s] z-[15] bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
+      :class="{'translate-x-[7rem] translate-y-[15rem] rotate-[-12deg] hover:scale-110 duration-[1s] cursor-pointer delay-[5.1s]': isClicked, 'delay-[0s]': isOnPosition }"
     ></div>
     <div
-    class="z-10 transition duration-500 bg-[#0055ff56] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 rotate-1"
+    class="z-10 transition duration-500 bg-[#0055ff] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 rotate-1"
     :class="{ 'scale-105': isHovered, 'translate-x-[-8rem] translate-y-[1rem] rotate-[-20deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class="z-20 transition duration-500 bg-[#00ff4845] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-1rem] translate-y-2 rotate-[-4deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
+    class="z-20 transition duration-500 bg-[#00ff48] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-1rem] translate-y-2 rotate-[-4deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
     :class="{ 'scale-105': isHovered, 'translate-x-[-2rem] translate-y-[0.5rem] rotate-[-7deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class="z-30 transition duration-500 bg-[#f9f94246] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-0.5rem] translate-y-2 rotate-[-2deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
+    class="z-30 transition duration-500 bg-[#f9f942] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-0.5rem] translate-y-2 rotate-[-2deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
     :class="{ 'scale-105': isHovered, 'translate-x-[4rem] translate-y-[0.5rem] rotate-[7deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class="z-40 transition duration-500 bg-[#ff003c39] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 translate-y-2 rotate-6 shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
+    class="z-40 transition duration-500 bg-[#ff003c] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 translate-y-2 rotate-6 shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
     :class="{ 'scale-105': isHovered, 'translate-x-[10rem] translate-y-[2rem] rotate-[20deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
@@ -144,7 +144,7 @@ export default {
       if (this.isOnPosition) {
         this.changePositionState();
       } else {
-        setTimeout(this.changePositionState, 5300)
+        setTimeout(this.changePositionState, 5200)
       }
       this.isClicked = !this.isClicked
     }
