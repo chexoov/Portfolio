eb7eb247dbeafabba6c371cc04e247a43c04e261
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-01-19 18:43:39 +0300

    added_shadows_to_big_cards

--- portfolio/src/components/Cards.vue
+++ portfolio/src/components/Cards.vue
@@ -20,29 +20,29 @@
       class="bg-[#ffffff27] w-20 aspect-square rounded-xl absolute"
     ></div>
     <div
-    class=" transition duration-500 bg-[#ffffff27] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 rotate-1"
-    :class="{ 'scale-105': isHovered, 'translate-x-[-7rem] translate-y-[1rem] rotate-[-20deg]': isClicked }"
+    class=" transition duration-500 bg-[#0055ff] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 rotate-1"
+    :class="{ 'scale-105': isHovered, 'translate-x-[-8rem] translate-y-[1rem] rotate-[-20deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class=" transition duration-500 bg-[#ffffff27] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-1rem] translate-y-2 rotate-[-4deg]"
-    :class="{ 'scale-105': isHovered, 'translate-x-[-4rem] translate-y-[0.5rem] rotate-[-7deg]': isClicked }"
+    class=" transition duration-500 bg-[#00ff48] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-1rem] translate-y-2 rotate-[-4deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
+    :class="{ 'scale-105': isHovered, 'translate-x-[-2rem] translate-y-[0.5rem] rotate-[-7deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class=" transition duration-500 bg-[#ffffff27] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-0.5rem] translate-y-2 rotate-[-2deg]"
+    class=" transition duration-500 bg-[#f9f942] w-48 aspect-[5/7] rounded-2xl absolute translate-x-[-0.5rem] translate-y-2 rotate-[-2deg] shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
     :class="{ 'scale-105': isHovered, 'translate-x-[4rem] translate-y-[0.5rem] rotate-[7deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
     ></div>
     <div
-    class=" transition duration-500 bg-[#ffffff27] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 translate-y-2 rotate-6"
-    :class="{ 'scale-105': isHovered, 'translate-x-[7rem] translate-y-[1rem] rotate-[20deg]': isClicked }"
+    class=" transition duration-500 bg-[#ff003c] w-48 aspect-[5/7] rounded-2xl absolute translate-x-1 translate-y-2 rotate-6 shadow-[-20px_10px_20px_rgba(0,0,0,0.25)]"
+    :class="{ 'scale-105': isHovered, 'translate-x-[10rem] translate-y-[2rem] rotate-[20deg]': isClicked }"
     @click="changeClickState()"
     @mouseover="changeHoverState()"
     @mouseleave="changeHoverState()"
