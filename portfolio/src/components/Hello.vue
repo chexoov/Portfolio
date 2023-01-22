078c44026c36f2670e3a717e02f2f0703a2870c1
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-01-22 20:30:52 +0300

    figered_out_animation

--- portfolio/src/components/Hello.vue
+++ portfolio/src/components/Hello.vue
@@ -1,14 +1,59 @@
 <template>
-  <!-- <h1 class=" text-white text-[5rem]">Привет, я Айнур</h1> -->
-  <section class="container">
-  <h1>
-    <span class="title">This is</span>
-    <span class="title">a long</span>
-    <span class="title">long title</span>
-  </h1>
+  <!-- This is a recreation of the SVG animation that can be found on Wokine.com -->
+
+<div class="hello-parent">
   
-  <div class="button">restart</div>
-</section>
+  <svg class="hello-word" width="365" height="365" viewBox="0 0 365 365">
+    
+    
+    <g id="H-letter">
+    <line class="H-left-stroke" x1="17" y1="17" x2="17" y2="104" stroke="#fff" fill="none" stroke-width="34" stroke-linecap="round" />
+    <line class="H-mid-stroke" x1="33" y1="10" x2="68" y2="10" stroke="#fff" fill="none" stroke-width="34" stroke-linecap="round"/>
+    <line class="H-mid-stroke" x1="33" y1="62" x2="68" y2="62" stroke="#fff" fill="none" stroke-width="34" stroke-linecap="round" />
+    <line class="H-right-stroke" x1="84" y1="17" x2="84" y2="104" stroke="#fff" fill="none" stroke-width="34" stroke-linecap="round" />
+    </g>
+    <g id="I-letter">
+      <circle class="O-stroke-i" cx="138" cy="20" r="5" stroke="#fff" fill="none" stroke-width="20" />
+    <line class="H-left-stroke" x1="138" y1="45" x2="138" y2="124" stroke="#fff" fill="none" stroke-width="34" />
+    </g>
+    
+    <!-- <g id="E-letter">
+      <line class="E-left-stroke" x1="138" y1="0" x2="138" y2="124" stroke="#000" fill="none" stroke-width="34" />
+      <line class="E-top-stroke" x1="154" y1="17" x2="201" y2="17" stroke="#000" fill="none" stroke-width="34" />
+      <line class="E-mid-stroke" x1="154" y1="62" x2="196" y2="62" stroke="#000" fill="none" stroke-width="34" />
+      <line class="E-bottom-stroke" x1="154" y1="107" x2="201" y2="107" stroke="#000" fill="none" stroke-width="34" />
+    </g> -->
+    
+    <g id="L-one-letter">
+      <line class="L-one-long-stroke" x1="17" y1="170" x2="17" y2="260" stroke="#fff" fill="none" stroke-width="34" />
+      <line class="L-one-short-stroke" x1="20" y1="260" x2="60" y2="260" stroke="#fff" fill="none" stroke-width="34" stroke-linecap="round" />
+    </g>
+    
+    <g id="L-two-letter">
+      <line class="L-two-long-stroke" x1="104" y1="153" x2="104" y2="277" stroke="#fff" fill="none" stroke-width="34" />
+      <line class="L-two-short-stroke" x1="120" y1="260" x2="164" y2="260" stroke="#fff" fill="none" stroke-width="34" />
+      <line class="L-one-long-stroke" x1="200" y1="153" x2="200" y2="260" stroke="#fff" fill="none" stroke-width="34" />
+
+    </g>
+    
+    <!-- <g id="O-letter">
+      <circle class="O-stroke" cx="231" cy="215" r="48" stroke="#fff" fill="none" stroke-width="31" />
+    </g> -->
+    
+    <g id="red-dot">
+      <!-- Initially I tried creating a circle but it was harder to manipulate it how I wanted to in CSS so I resorted to using a line trick to make it look like a circle ....
+      
+      <circle class="red-dot" cx="325" cy="260" r="20" fill="#FF5851" stroke="none" />
+  
+      -->
+      
+      <line x1="325" y1="260" x2="325" y2="260" stroke="#FF5851" class="red-dot" />
+    </g>
+    
+    
+  </svg>
+  </div>
+  
 </template>
 
 
@@ -20,76 +65,313 @@
 
 
 <style scoped>
-:root {
-  --shadow: #533d4a;
-  --text-color: white;
-}
-@import url(https://fonts.googleapis.com/css?family=Fjalla+One);
-
-html{
+html, body, .hello-parent {
   height: 100%;
-}
-
-body{
-  font-family: 'Fjalla One', sans-serif;
-  background: black;
-}
-
-.container{
-  /* transform: translate(-50%, -50%); */
-  /* top: 50%;
-  left: 50%; */
-  /* display: block; */
-  /* position: absolute; */
-  max-width: 225px;
-}
-
-.button{
-  float: left;
-  position: relative;
-  bottom: -65px;
-  left: 50%;
-  transform: translateX(-50%) rotate(-10deg);
-  color: white;
-  text-transform: uppercase;
-  opacity: 0;
-  visibility: hidden;
-  cursor: pointer;
-}
-  
-.button  span{
-    transform: skew(-10deg);
-    display: block;
-    float: left;
-    text-shadow: var(--shadow) 1px 1px, var(--shadow) 2px 2px, var(--shadow) 3px 3px, var(--shadow) 4px 4px;
-  }
-
-h1{
-  color: #fff;
-  text-transform: uppercase;
-  font-size: 42px;
+  width: 100%;
   margin: 0;
-  line-height: 47px;
-  letter-spacing: 2px;
 }
 
-.title{
-  transform: translateX(-50%) rotate(-10deg);
-  display: block;
-  float: left;
-  left: 50%;
-  position: relative;
-  color: white;
-  
+.hello-parent {
+  display: flex;
+  background: #fff;
+  background: -webkit-radial-gradient(#fff, #eaeaea); 
+  background: -o-radial-gradient(#fff, #eaeaea); 
+  background: -moz-radial-gradient(#fff, #eaeaea);
+  background: radial-gradient(#fff, #eaeaea); 
 }
-span {
-    transform: skew(-20deg);
-    display: block;
-    float: left;
-    text-shadow: var(--shadow) 1px 1px, var(--shadow) 2px 2px, var(--shadow) 3px 3px, var(--shadow) 4px 4px, var(--shadow) 5px 5px, var(--shadow) 6px 6px;
-    min-width: 10px;
-    min-height: 10px;
-    position: relative;
+
+.hello-word {
+  margin:auto;
+  background-color: rgb(0, 0, 0);
+  border: #fff 1px solid;
+}
+/* H Animation */
+
+.H-left-stroke {
+  stroke-dasharray: 87px;
+  /* stroke-dashoffset: 124; */
+  animation: H-left-move 40s ease forwards;
+}
+
+.H-mid-stroke {
+  stroke-dasharray: 37px;
+  stroke-dashoffset: 37px;
+  animation: H-mid-move 20s ease forwards;
+}
+
+.H-right-stroke {
+  stroke-dasharray: 124px;
+  stroke-dashoffset: 104px;
+  animation: H-right-move 40s ease forwards;
+}
+
+@keyframes H-left-move {
+  0% {
+    stroke-dashoffset: 87px;
   }
+  5% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
 
+@keyframes H-mid-move {
+  0% {
+    stroke-dashoffset: 37px;
+  }
+  5% {
+    stroke-dashoffset: 37px;
+  }
+  10% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes H-right-move {
+  0% {
+    stroke-dashoffset: 124px;
+  }
+  5% {
+    stroke-dashoffset: 124px;
+  }
+  10% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+/* E Animation */
+
+.E-left-stroke {
+  stroke-dasharray: 124px;
+  stroke-dashoffset: 124px;
+  animation: E-left-move 20s ease forwards;
+}
+
+.E-top-stroke {
+  stroke-dasharray: 47px;
+  stroke-dashoffset: 47px;
+  animation: E-top-move 10s ease forwards;
+}
+
+.E-mid-stroke {
+  stroke-dasharray: 42px;
+  stroke-dashoffset: 42px;
+  animation: E-mid-move 10s ease forwards;
+}
+
+.E-bottom-stroke {
+  stroke-dasharray: 47px;
+  stroke-dashoffset: 47px;
+  animation: E-bottom-move 10s ease forwards;
+}
+
+@keyframes E-left-move {
+  0% {
+    stroke-dashoffset: 124px;
+  }
+  2% {
+    stroke-dashoffset: 124px;
+  }
+  6% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes E-top-move {
+  0% {
+    stroke-dashoffset: 47px;
+  }
+  6% {
+    stroke-dashoffset: 47px;
+  }
+  11% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes E-mid-move {
+  0% {
+    stroke-dashoffset: 42px;
+  }
+  8% {
+    stroke-dashoffset: 42px;
+  }
+  13% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes E-bottom-move {
+  0% {
+    stroke-dashoffset: 47px;
+  }
+  11% {
+    stroke-dashoffset: 47px;
+  }
+  16% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+/* L One Animation */
+
+.L-one-long-stroke {
+  /* border-radius: 50px; */
+  stroke-dasharray: 124px;
+  stroke-dashoffset: 124px;
+  stroke-linecap: round;
+  animation: L-one-long-move 20s ease forwards;
+}
+
+.L-one-short-stroke {
+  stroke-dasharray: 44px;
+  stroke-dashoffset: 44px;
+  animation: L-one-short-move 10s ease forwards;
+}
+
+@keyframes L-one-long-move {
+  0% {
+    stroke-dashoffset: 124px;
+  }
+  2% {
+    stroke-dashoffset: 124px;
+  }
+  7% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes L-one-short-move {
+  0% {
+    stroke-dashoffset: 44px;
+  }
+  13% {
+    stroke-dashoffset: 44px;
+  }
+  18% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+/* L Two Animation */
+
+.L-two-long-stroke {
+  stroke-dasharray: 124px;
+  stroke-dashoffset: 124px;
+  animation: L-two-long-move 20s ease forwards;
+}
+
+.L-two-short-stroke {
+  stroke-dasharray: 44px;
+  stroke-dashoffset: 44px;
+  animation: L-two-short-move 10s ease forwards;
+}
+
+@keyframes L-two-long-move {
+  0% {
+    stroke-dashoffset: 124px;
+  }
+  3% {
+    stroke-dashoffset: 124px;
+  }
+  8% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+@keyframes L-two-short-move {
+  0% {
+    stroke-dashoffset: 44px;
+  }
+  15% {
+    stroke-dashoffset: 44px;
+  }
+  20% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+/* O Animation */
+
+.O-stroke {
+  stroke-dasharray: 302px;
+  stroke-dashoffset: 302px;
+  animation: O-move 20s ease forwards;
+}
+
+.O-stroke-i {
+  stroke-dasharray: 302px;
+  stroke-dashoffset: 302px;
+  animation: O-move 50s ease forwards;
+}
+
+@keyframes O-move {
+  0% {
+    stroke-dashoffset: 302px;
+  }
+  4% {
+    stroke-dashoffset: 302px;
+  }
+  9% {
+    stroke-dashoffset: 0px;
+  }
+  100% {
+    stroke-dashoffset: 0px;
+  }
+}
+
+/* Red Dot Animation */
+
+.red-dot {
+  stroke-width: 44px;
+  stroke-linecap: round;
+  animation: red-dot-grow 8s ease-out forwards;
+}
+
+@keyframes red-dot-grow {
+  0% {
+    stroke-width: 0px;
+  }
+  15% {
+    stroke-width: 0px;
+  }
+  20% {
+    stroke-width: 44px;
+  }
+  100% {
+    stroke-width: 44px;
+  }
+}
 </style>
